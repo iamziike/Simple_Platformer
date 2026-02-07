b912073120ca0aa4529769d508ea4aa1b61d058f
Wisdom Alaje <93620858+iamziike@users.noreply.github.com>
2026-02-07 11:23:01 +0000

    refactor: update folder structure

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class Constants
+{
+    static class Tag
+    {
+        public const string Player = "Player";
+    }
+}
